796eed2906af81e6d669789808e19710845d35ac
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2023-12-16 15:20:40 +0800

    feat: Create another default completions api

---
+++ server/api/summary.ts
@@ -0,0 +1,21 @@
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from 'openai/resources/chat';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+export default defineLazyEventHandler(() => {
+    const apiKey = useRuntimeConfig().openaiApiKey;
+    if (!apiKey) throw new Error('Missing OpenAI API key');
+    const openai = new OpenAI({
+        apiKey: apiKey,
+    });
+
+    return defineEventHandler(async (event) => {
+        const messages = await readBody<ChatCompletionMessageParam[]>(event)
+        const response = await openai.chat.completions.create({
+            model: 'gpt-4',
+            messages,
+        });
+
+        return response.choices[0].message.content
+    })
+})

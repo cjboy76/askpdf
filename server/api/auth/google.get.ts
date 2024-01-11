2169330f23d509326d438c1cb086fd9b0c6c858a
CJBoy <78361904+cjboy76@users.noreply.github.com>
2024-01-11 16:25:38 +0800

    fix: OAuth 調整

--- server/api/auth/google.get.ts
+++ server/api/auth/google.get.ts
@@ -1,37 +1,22 @@
-import { User } from '../../models/userModel'
-
 declare module '#auth-utils' {
-    interface UserSession {
-        sub: string
-        name: string
-        email: string
-        picture: string
-        locale: string
-    }
+  interface UserSession {
+    sub: string
+    name: string
+    email: string
+    picture: string
+    locale: string
+  }
 }
-export { }
+export {}
 
 export default oauth.googleEventHandler({
-    config: {
-        scope: ['email', 'profile'],
-        redirectUrl: '/auth/google'
-    },
-    async onSuccess(event, { user }) {
-        try {
-            await User.create({
-                sub: user.sub,
-                name: user.name,
-                email: user.email,
-                picture: user.picture,
-                locale: user.locale
-            })
-        }
-        catch (err) {
-            console.log('err', err)
-        }
-
-        // @ts-expect-error package type issue
-        await setUserSession(event, { user, loggedInAt: new Date() })
-        return sendRedirect(event, '/')
-    },
+  config: {
+    scope: ['email', 'profile'],
+    redirectUrl: '/auth/google'
+  },
+  async onSuccess(event, { user }) {
+    // @ts-expect-error package type issue
+    await setUserSession(event, { user })
+    return sendRedirect(event, '/')
+  }
 })
--- server/models/userModel.ts
+++
@@ -1,9 +0,0 @@
-import { defineMongooseModel } from '#nuxt/mongoose'
-
-export const User = defineMongooseModel('User', {
-  name: String,
-  email: String,
-  sub: String,
-  picture: String,
-  locale: String
-})

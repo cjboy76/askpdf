2a3fad398c1f764ef1546392e0f812e39a435d11
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2024-02-24 23:12:26 +0800

    fix: Update prev vector ids

--- server/api/deleteVector.post.ts
+++ server/api/deleteVector.post.ts
@@ -3,12 +3,9 @@ import { usePinecone } from '../utils'
 export default defineEventHandler(async (event) => {
   const { ids } = await readBody<{ ids: string[] }>(event)
   try {
-    const result = await usePinecone(event).delete(ids)
+    const result = await usePinecone(event).delete({ ids })
     return result
   } catch (error: unknown) {
-    throw createError({
-      statusCode: (error as any).status,
-      statusMessage: (error as any).error.message
-    })
+    console.log(error)
   }
 })

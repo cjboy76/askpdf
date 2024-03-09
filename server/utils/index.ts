9baf317e3374a8502ce236d22ab1745af908bc6f
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2024-03-09 11:28:19 +0800

    wip: pinecone server instance

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -5,28 +5,23 @@ import type { EventHandlerRequest, H3Event } from 'h3'
 import OpenAI from 'openai'
 
 export function usePinecone(event: H3Event<EventHandlerRequest>) {
-  let _pineconeStore: PineconeStore | null = null
   const apiKey =
     process.env.NUXT_OPENAI_API_KEY ||
     event.node.req.headers['x-openai-key']?.toString() ||
     ''
-  if (!_pineconeStore) {
-    try {
-      const pineconeIndex = useRuntimeConfig().pineconeIndex
-      const pc = new Pinecone()
-      const pcIndex = pc.Index(pineconeIndex)
+  try {
+    const pineconeIndex = useRuntimeConfig().pineconeIndex
+    const pc = new Pinecone()
+    const pcIndex = pc.Index(pineconeIndex)
 
-      const embeddings = new OpenAIEmbeddings({
-        openAIApiKey: apiKey
-      })
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: apiKey
+    })
 
-      _pineconeStore = new PineconeStore(embeddings, { pineconeIndex: pcIndex })
-    } catch {
-      throw new Error('No database configured for production')
-    }
+    return new PineconeStore(embeddings, { pineconeIndex: pcIndex })
+  } catch {
+    throw new Error('No database configured for production')
   }
-
-  return _pineconeStore
 }
 
 export function useOpenAI(event: H3Event<EventHandlerRequest>) {

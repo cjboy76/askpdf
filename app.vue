ac21f659bd353fe83013226032d8dff9f0b72972
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2024-04-24 16:39:52 +0800

    wip: meta tag

--- app.vue
+++ app.vue
@@ -4,8 +4,25 @@
   </NuxtLayout>
 </template>
 
-<style>
+<script setup>
+useHead({
+  title: 'AI PDF 閱讀小工具',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'A AI PDF Reader App with integrated chat, read and discuss in real time.'
+    },
+    {
+      name: 'description',
+      content:
+        'AI PDF 閱讀小工具，提供實時對話、閱讀與討論'
+    }
+  ]
+})
+</script>
 
+<style>
 html {
   background-color: rgb(39 39 42);
 }

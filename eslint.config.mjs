6c1dd2a9109d311fed416cea8cd4e180c705a71a
CJBoy <78361904+cjboy76@users.noreply.github.com>
2024-10-31 11:48:24 +0800

    fix: replaced linter plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,6 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
-    'sort-imports': ['error', {
-      ignoreCase: false,
-      ignoreDeclarationSort: false,
-      ignoreMemberSort: false,
-      memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-      allowSeparatedGroups: false,
-    }],
+    'import/order': 'error'
   },
 })
